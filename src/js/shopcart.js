61fcc5e0f4815a028594af7bdf41cccaf6bcc464
Domyself <1728514647@qq.com>
2019-04-11 13:36:44 +0800

    购物车选框&js压缩功能的关闭

--- src/js/shopcart.js
+++ src/js/shopcart.js
@@ -9,20 +9,62 @@ require(["require.config"],function(){
                 //渲染列表
                 let data = localStorage.getItem("cart");
                 if(data){
+                    // console.log($(".cart-empty"));
+                    $(".cart-empty").css("display","none");
                     data = JSON.parse(data);
+                    this.sLength = data.length;
                     this.container.html(template("cart-list",{list: data}));
                 }  
+
                 // 绑定事件
-                console.log( $(".icon-checked"));
-                $(".icon-checked").toggle(
-                    function () {
-                        // console.log(1111);
-                      $(this).addClass("selected");
-                    },
-                    function () {
-                      $(this).removeClass("selected");
+                //单选
+                this.check();
+                //全选
+                this.allcheck();
+                console.log("easy");                
+
+            }
+            check(){
+                this.checkLength = 0;
+                var _this = this;
+                $(".check").on("click",function(){
+                    if($(this).get(0).className.includes("selected")){
+                        _this.checkLength--;
+                        $(this).removeClass("selected");
+                        // console.log(_this.checkLength);
+                    }else{
+                        $(this).addClass("selected");
+                        _this.checkLength++;
+                        // console.log(_this.checkLength);
+                    } 
+                    if(_this.checkLength === _this.sLength){
+                        $("#allcheck").addClass("selected");
+                    }else{
+                        $("#allcheck").removeClass("selected");
                     }
-                  );
+                })
+            }
+            allcheck(){
+                var _this = this;
+                $("#allcheck").on("click",function(){
+                    if($(this).get(0).className.includes("selected")){
+                        $(".icon-checked").get().forEach(element => {
+                            _this.checkLength = 0;
+                            element.classList.remove("selected");
+                            // console.log(_this.checkLength);
+                        });
+                    }else{
+                        $(".icon-checked").get().forEach(element =>{
+                            _this.checkLength = _this.sLength;
+                            element.classList.add("selected");
+                            // console.log(_this.checkLength);                              
+                        })
+                    }  
+                })
+            }
+
+            subtotal(){
+
             }
 
         }
