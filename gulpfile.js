0e39c121f4fc35b6122aaea0dbbe6e80c04004d0
Domyself <1728514647@qq.com>
2019-04-12 20:25:43 +0800

    列表页排序

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task("js", () => {
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    // .pipe(uglify())
+    .pipe(uglify())
     .pipe(gulp.dest("dist/js"))
     .pipe(connect.reload());
 })
--- src/js/list.js
+++ src/js/list.js
@@ -7,6 +7,8 @@ require(["require.config"],function(){
             init(){
                 this.toprank();
                 this.list();
+                this.priceBtn();
+                this.salesBtn();
             }
             toprank(){
                 // 请求热门排行数据
@@ -14,11 +16,64 @@ require(["require.config"],function(){
 				new Toprank($("#toprankContainer"),url.baseUrl + "list-toprank");
             }
             list(){
-				//请求商品数据
-				// console.log(url);
-				// console.log(ShopItem);
+				//请求商品数据    
 				new ShopItem($("#hotListContainer"),url.baseUrl + "list-goods");
-			}
+            }
+            salesBtn(){
+                let i = 0
+                $("#salsBtn").on("click",function(){
+                    $(this).css("color","#f74a4a");
+                    i++;
+                    if(i%2 === 0){
+                        $("#s-gt").attr("style","background-position:-25px -71px");
+                    }else{
+                        $("#s-gt").attr("style","background-position:-40px -71px");
+                    }
+                    new ShopItem($("#hotListContainer"),url.baseUrl + "list-goods");
+                })   
+            }
+            priceBtn () {
+                // 排序
+                let i = 0;
+                $("#priceBtn").on("click", function () {
+                    $(this).css("color","#f74a4a");
+                  i++;
+                  if(i%2 != 0){
+                    //   console.log($("#p-gt"));
+                      $("#p-gt").attr("style","background-position:-25px -71px");
+                    new Promise(resolve => {
+                        $.get(url.baseUrl+"list-goods", res => {
+                          if(res.res_code === 1){
+                            resolve(res.res_body.list);
+                          }
+                        })
+                      }).then(list => {
+                        // 升序
+                        list = list.sort((a,b) => {
+                          return a.qprice-b.qprice;
+                        })
+                        new ShopItem($("#hotListContainer"),"",list);
+                      })
+                  }else{
+                    $("#p-gt").attr("style","background-position:-40px -71px");
+                    new Promise(resolve => {
+                        $.get(url.baseUrl+"list-goods", res => {
+                          if(res.res_code === 1){
+                            resolve(res.res_body.list);
+                          }
+                        })
+                      }).then(list => {
+                        // 降序
+                        list = list.sort((a,b) => {
+                          return b.qprice-a.qprice;
+                        })
+                        new ShopItem($("#hotListContainer"),"",list);
+                      })
+                  }
+                  
+                  
+                })
+              }
         }
         new List();
     })
